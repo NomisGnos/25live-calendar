e2c18a1678bc8fb90bd558c7c8546243b16a2639
Simon Song <nomisgnos@hotmail.com>
2021-06-11 15:16:49 -0400

    init

---
+++ js/scripts.js
@@ -0,0 +1,106 @@
+$(".psucampus_25livespudembed").ready(function(){
+	console.log('s');
+if ($('.psucampus_25livespudembed').length > 0) {
+	console.log('s');
+  var spudScriptUrl = "https://25livepub.collegenet.com/scripts/spuds.js";
+  $.getScript(spudScriptUrl).done(function(){
+
+	// Only do this for "News Style"
+	$(".psucampus_25livespudembed[spudtype=newsstyle]").each(function(){
+	  var spudId = $(this).attr("spudid");
+	  var uniqueId = $(".twentyFiveliveModal", this).data("uniqueid");
+	  var APIurl = "//25livepub.collegenet.com/calendars/" + spudId + ".json?unique=" + Math.random();
+	  $.getJSON(APIurl, function( response ) {
+		var arrEventId = [];
+		$.each(response, function(rowNum, rowData) {
+		  arrEventId.push(rowData.eventID);
+		  var startDate = new Date(rowData.startDateTime);
+		  var endDate = new Date(rowData.endDateTime);
+		  $(".psucampus_25livespudembed[spudid='" + spudId + "'][spudtype=newsstyle]:has(div#twentyFiveliveModal_" + uniqueId + ")").append('\
+			<div id="25live-eventid-' + rowData.eventID + '" class="psu-node--teaser node-type--event clearfix" style="background-color: transparent !important;">\
+			  <div class="calendar-icon-container">\
+				<div class="calendar-icon">\
+				  <div class="month">' + startDate.toLocaleDateString("en-EN", { month: "short" }) + '</div>\
+				  <div class="day">' + startDate.toLocaleDateString("en-EN", { day: "numeric" }) + '</div>\
+				  <div class="day-of-week">' +
+					startDate.toLocaleDateString("en-EN", { weekday: "short" }) +
+				  '</div>\
+				</div>\
+			  </div>\
+			  <div class="content teaser-content">\
+				<h2 class="node-title"><a href="?trumbaEmbed=view%3Devent%26eventid%3D' + rowData.eventID + '&#anchor_twentyFiveliveModal_' + spudId + '" onclick="location.reload()">' + rowData.title + '</a></h2>\
+				<div>' + 
+				  startDate.toLocaleDateString("en-EN", { weekday: "short" })                                 + ', ' + 
+				  startDate.toLocaleDateString("en-EN", { month: "short" })                                   + ' ' + 
+				  startDate.toLocaleDateString("en-EN", { day: "numeric" })                                   + ', ' + 
+				  startDate.toLocaleTimeString("en-EN", { hour: "numeric", minute: "numeric", hour12: true }).toLowerCase().replace("am","a.m.").replace("pm","p.m.") + ' to ' + 
+				  endDate.toLocaleTimeString("en-EN", { hour: "numeric", minute: "numeric", hour12: true }).toLowerCase().replace("am","a.m.").replace("pm","p.m.")   + ' \
+				</div>\
+			  </div>\
+			</div>\
+		  '); //end of append()
+		}); // end of each()
+	  
+		if ( getEventIdFromQueryString() != null && arrEventId.includes(getEventIdFromQueryString()) ) {
+		  $("#twentyFiveliveModal_" + uniqueId ).addClass("turnOnDetails");
+		  $("#spud_" + spudId + "_details_" + uniqueId).addClass("turnOnDetails");
+		  $Trumba.addSpud({
+			webName: spudId,
+			spudType : "main",
+			spudId : "spud_" + spudId + "_details_" + uniqueId,
+			url : { template : "month" },
+		  });
+		}
+	  }); // end of getJSON
+	}); // end of "psucampus_25livespudembed".once.each
+  }); // end of "getScript.done()"
+} // end of IF statement that "psucampus_25livespudembed" exists
+}); // end psucampus_25livespudembed.ready
+
+// Get ALL 25live modal
+var modal = $(".twentyFiveliveModal");
+
+// When the user clicks on <span> (x), close the modal
+$(".twentyFiveliveModal .close").on("click", function() {
+modal.css("display", "none");
+});
+
+// When the user clicks anywhere outside of the modal, close it
+modal.on("click", function(event) {
+if ($(event.target).hasClass("twentyFiveliveModal")) {
+  modal.css("display", "none");
+}
+});
+
+/*
+* Toggles 25live spud between month/list view.  This is only for ADAPTIVE mode.
+*/
+function showHideTrumba(spudid) {
+var spudwidth = jQuery(".psucampus_25livespudembed[spudid=" + spudid + "]").outerWidth();
+if ( spudwidth > 500) {
+  jQuery("#spud_" + spudid + "_month").show();
+  jQuery("#spud_" + spudid + "_table").hide();
+}
+else{
+  jQuery("#spud_" + spudid + "_month").hide();
+  jQuery("#spud_" + spudid + "_table").show();
+}
+}
+
+/*
+* Gets 25Live Event ID from the query string to display as a SPUD
+*/
+function getEventIdFromQueryString() {
+var urlParams = new URLSearchParams(window.location.search);
+var param = urlParams.get("trumbaEmbed");
+if (param != null) {
+  var vars = param.split("&");
+  for (var i = 0; i < vars.length; i++) {
+	  var pair = vars[i].split("=");
+	  if (decodeURIComponent(pair[0]) == "eventid") {
+		  return parseInt(decodeURIComponent(pair[1]));
+	  }
+  } 
+}
+return null; 
+}
